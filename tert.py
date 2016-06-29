08bdfb8d8517e2fd07c32050c3a3048e6fae4cd4
unknown <student@basm-509-std09.staff.corp.local>
2016-06-29 10:53:28 +0300

    commit

---
+++ tert.py
@@ -0,0 +1,36 @@
+import re
+
+def readfile ():
+    filename = input ('Print the way to the file ')
+    ff = open (filename, 'r', encoding = 'utf-8')
+    f = ff.readlines ()
+    ff.close
+    return f
+
+def lookfornames (f):
+    regex = ' ([А-Я]\. [А-Я][а-я]+)'
+    for line in f:
+        res = re.findall (regex, line)
+        for elem in res:
+            print (elem)
+
+def lookagain (f):
+    regex2 = '[А-Я].\. [А-Я].\. [А-Я][а-я]+'
+    regex3 = '[А-Я][а-я]+ [А-Я][а-я]+'
+    for line in f:
+        res2 = re.findall (regex2, line)
+        res3 = re.findall (regex3, line)
+        for name in res2:
+            print (name)
+        for match in res3:
+            print (match)
+    
+def main ():
+    fun1 = readfile ()
+    fun2 = lookfornames (fun1)
+    fun3 = lookagain (fun1)
+
+if __name__ == '__main__':
+    main ()
+        
+        
